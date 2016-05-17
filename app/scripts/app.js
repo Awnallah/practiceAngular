5758d7be05fd58e2ed53f2e9c3a3bc7f003b284d
Hamzah <6hamzah6@gmail.com>
2016-05-16 19:56:55 -0700

    contact form controller added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,4 +71,14 @@ angular.module('confusionApp', [])
                 $scope.showDetails = !$scope.showDetails;
             };
 
+        }])
+
+      .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                               agree:false, email:"" };
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
         }]);
